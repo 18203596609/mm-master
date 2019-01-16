434bcb630027063a15f8a52dbde4d37801aff035
guomm3 <guomm3@lenovo.com>
2019-01-16 17:25:24 +0800

    head部分完成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeadComponent } from './component/head/head.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
+import { LeftContentComponent } from './component/left-content/left-content.component';
+import { CenterContentComponent } from './component/center-content/center-content.component';
+import { RightContentComponent } from './component/right-content/right-content.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    HomePageComponent,
+    LeftContentComponent,
+    CenterContentComponent,
+    RightContentComponent
   ],
   imports: [
     BrowserModule
