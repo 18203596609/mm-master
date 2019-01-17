6850bf029952fcab768fd9eea58ad400bf780049
guomm3 <guomm3@lenovo.com>
2019-01-17 18:17:05 +0800

    引用ng-zorro框架，left-cont左边菜单栏布局

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgZorroAntdModule } from 'ng-zorro-antd'; // 引入ng-zorro
 
 
 import { AppComponent } from './app.component';
@@ -8,6 +10,10 @@ import { HomePageComponent } from './component/home-page/home-page.component';
 import { LeftContentComponent } from './component/left-content/left-content.component';
 import { CenterContentComponent } from './component/center-content/center-content.component';
 import { RightContentComponent } from './component/right-content/right-content.component';
+
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+registerLocaleData(zh);
 
 
 @NgModule({
@@ -20,9 +26,12 @@ import { RightContentComponent } from './component/right-content/right-content.c
     RightContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    /** 导入 ng-zorro-antd 模块 **/
+    NgZorroAntdModule.forRoot()
   ],
-  providers: [],
+  providers   : [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/component/left-content/left-content.component.ts
+++ src/app/component/left-content/left-content.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left-content.component.scss']
 })
 export class LeftContentComponent implements OnInit {
+  content: any = ['快速上手', '教程', '核心知识', '其它技术',
+   '环境搭建与部署', '发布信息', '快捷手册', 'API参考手册'];
+   jcCont: any = ['简介', '应用的"外壳"', '1.英雄编辑器', '2.显示英雄列表',
+   '3.主从组件', '4.服务', '5.路由', '6.HTTP'];
+   zcCont: any = ['架构', '组件与模版', '表单', 'Observable与RxJS',
+   '引导启动', 'Angular模块', '依赖注入', 'HttpClient', '路由与导航', '动画'];
+   openMap = {
+    sub1: false,
+    sub2: false,
+    sub3: false
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openHandler(value: string): void {
+    for (const key in this.openMap) {
+      if (key !== value) {
+        this.openMap[ key ] = false;
+      }
+    }
   }
 
 }
